0810fbdc8f190601d0e4eb0af359b291074095d0
rouzbeh-afrasiabi <rouzbeh.afrasiabi@gmail.com>
2019-10-15 10:34:29 -0400

    Update test_one.py

--- tests/test_one.py
+++ tests/test_one.py
@@ -4,7 +4,7 @@ import torch
 class TestClass:
     def test_one(self):
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        assert device =="cpu"
+        assert device.type=='cpu'
 
     def test_two(self):
         x = 1
