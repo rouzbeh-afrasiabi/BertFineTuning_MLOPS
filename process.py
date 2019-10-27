1cea3f88aa942c04d7e5554aa05f0d917f1313e4
Rouzbeh Afrasiabi <34456532+rouzbeh-afrasiabi@users.noreply.github.com>
2019-10-26 23:29:53 -0400

    Update process.py

--- process.py
+++ process.py
@@ -4,7 +4,7 @@ from azureml.core import Run
 def get_args():
 
   parser = argparse.ArgumentParser()
-  parser.add_argument("--def_blob_store",  type=str, dest="def_blob_store")
+  parser.add_argument("--input_data_ref",  type=str, dest="input_data_ref")
   args = parser.parse_args()
 
   return(args)
@@ -14,7 +14,4 @@ if __name__ == '__main__':
     global run
     run = Run.get_context()
     _params=get_args()
-    def_blob_store=_params.def_blob_store
-    blob_container_name=def_blob_store.container_name
-    
-    input_data_ref=def_blob_store.blob_service.get_blob_to_text(container_name=blob_container_name,blob_name='data/original/train.csv')
+    input_data_ref=_params.input_data_ref
