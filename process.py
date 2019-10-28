ef46b936c6e2d7056ed489b7086d34ec5b29d10e
Rouzbeh Afrasiabi <34456532+rouzbeh-afrasiabi@users.noreply.github.com>
2019-10-28 19:15:52 -0400

    Update process.py

--- process.py
+++ process.py
@@ -45,8 +45,8 @@ sys.path.insert(0, cwd)
 def get_args():
 
   parser = argparse.ArgumentParser()
-#   parser.add_argument("--processed_data_ref",  type=str, dest="processed_data_ref")
-#   parser.add_argument("--input_data_ref",  type=str, dest="input_data_ref")
+  parser.add_argument("--processed_data_ref",  type=str, dest="processed_data_ref")
+  parser.add_argument("--input_data_ref",  type=str, dest="input_data_ref")
   parser.add_argument("--tenant_id",  type=str, dest="tenant_id")
   parser.add_argument("--application_id",  type=str, dest="application_id")
   parser.add_argument("--app_secret",  type=str, dest="app_secret")
