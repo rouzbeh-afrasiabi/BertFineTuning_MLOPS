6b2a9b4239c9bce16abbad8467fe3041070809b7
Rouzbeh Afrasiabi <34456532+rouzbeh-afrasiabi@users.noreply.github.com>
2019-10-29 09:22:41 -0400

    Update process.py

--- process.py
+++ process.py
@@ -94,4 +94,5 @@ if __name__ == '__main__':
     
     processed_data_ref=_params.processed_data_ref
     input_data_ref=_params.input_data_ref
-    pd.read_csv(os.path.join((input_data_ref,'original','train.csv')))
+    pd.read_csv('{}/original/train.csv'.format(input_data_ref))
+    
